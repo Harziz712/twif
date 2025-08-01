82223cdae82ac78ad5bd1f3ae8e2d82fd5a40887
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-08-02 00:51:31 +0100

    add: Navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="top-0 h-30 bg-gray-950  text-amber-50 fixed w-full">navbar</div>
+  )
+}
+
+export default Navbar
