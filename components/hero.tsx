d41bf4e42cbdf0b06dbb2343a96a5bf6fd139741
Akanbi AbdulAzeez <akanbiazeez117@gmail.com>
2025-08-07 05:42:40 +0100

    refactoring

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,17 +5,25 @@ import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { motion } from "framer-motion";
 
-const slides = [
+interface SlideProps {
+    image: string,
+    title: string,
+    subtitle: string,
+    plus?:string
+}
+
+const slides: SlideProps[] = [
   {
     image: "/hero1.jpg",
-    title: "Dress the real you",
-    subtitle: `Discover TWIF's bespoke and ready-to-wear collection.
-     Clothes made to fit you, not the other way around`,
+    title: "Shop Men's Suits",
+    subtitle: `Discover TWIF's Clothings for Premium bespoke and ready-to-wear men's clothing in Nigeria`,
+    plus: "Explore our collection of suits, agbada, native attire, and more.",
   },
     {
     image: "/native.png",
     title: "Traditional Meets Modern",
     subtitle: "Native attire redesigned for today’s man",
+    plus: "Experience the fusion of tradition and contemporary fashion.",
   },
   {
     image: "/elegance.png",
@@ -68,24 +76,34 @@ export default function HeroCarousel() {
               className="text-5xl md:text-7xl font-semibold mb-4"
             >
               {slide.title.split(" ").map((word, idx) => (
-                <motion.span
+                <motion.h1
                   variants={wordAnim}
                   key={idx}
                   className="inline-block mr-2"
                 >
                   {word}
-                </motion.span>
+                </motion.h1>
               ))}
             </motion.div>
 
-            <motion.p
+            <motion.h2
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.8 }}
-              className="text-xl md:text-2xl font-light max-w-2xl"
+              className="text-xl md:text-2xl font-medium text-gray-200 max-w-2xl"
             >
               {slide.subtitle}
-            </motion.p>
+            </motion.h2>
+            
+            <motion.a
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 1 }}
+              href="#"
+              className="text-lg md:text-xl bottom-10  right-0 bg-gray-900/60 px-5 rounded-bl-2xl rounded-tl-2xl absolute font-medium text-gray-300 hover:text-gray-400 max-w-2xl"
+            >
+              {slide.plus}
+            </motion.a>
           </div>
         </div>
       ))}
